6b863ea7ab3486ad9bb8a400e76dbd59838d86c9
Daniel Springvledt <danispringveldt@gmail.com>
2026-06-29 02:39:04 +0200

    background color and routing

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <div className={`App ${viewportClass}`}>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Home" element={<Home />} />
             <Route path="/Brands" element={<Brands />} />
             <Route path="/CarStats" element={<CarStats />} />
             <Route path="/Results" element={<SearchResults />} />
